862b34c3b938aefc84d3cb5abbc14187456d6300
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2022-11-10 16:36:24 -0600

    Add timeblock in index and js

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var date = dayjs().format("H A");
+
+
+$("#currentDay").text(date);
+
+var timeCurrently = dayjs().hour()
+
+//funtion to save textbox content to local storage
+function saveLocal(){
+    var textbox = $(".textbox").val()
+    var hour = $(".hour").val()
+    localStorage.setItem(textbox, hour);
+}
+
+//button listner
+$(".saveBtn").on("click", saveLocal)
+
+//recipe for how to create a time block and inject in .container element
+var div1 = $("<div>").addClass("row time-block")
+var div2 = $("<div>").addClass("col-md-1 hour")
+var textarea = $("<textarea>").addClass("col-md-10 textbox future")
+var button = $("<button>").addClass("btn saveBtn col-md-1")
+
+button.append("<i class='fa fa-save'>")
+div1.append(div2).append(textarea).append(button);
+$(".container").append(div1);
