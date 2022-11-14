305db1f89b4bd90982da9328538c0b1375b43987
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2022-11-14 15:43:16 -0600

    Add for loop for timeblocks

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ $("#currentDay").text(date);
 var timeCurrently = dayjs().hour()
 
 //funtion to save textbox content to local storage
-function saveLocal(){
+function saveLocal() {
     var textbox = $(".textbox").val()
     var hour = $(".hour").val()
     localStorage.setItem(textbox, hour);
@@ -24,3 +24,18 @@ var button = $("<button>").addClass("btn saveBtn col-md-1")
 button.append("<i class='fa fa-save'>")
 div1.append(div2).append(textarea).append(button);
 $(".container").append(div1);
+
+//for loop 8x
+//JSON.parse(localStorage.getItem(""))
+for (let i = 0; i < 8; i++) {
+    var div1 = $("<div>").addClass("row time-block")
+    var div2 = $("<div>").addClass("col-md-1 hour")
+    var textarea = $("<textarea>").addClass("col-md-10 textbox future")
+    var button = $("<button>").addClass("btn saveBtn col-md-1")
+    button.append("<i class='fa fa-save'>")
+    div1.append(div2).append(textarea).append(button);
+    $(".container").append(div1);
+}
+
+
+
