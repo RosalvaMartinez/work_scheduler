2d61b985387bf58c44003ccbe00187edaec6067d
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2022-11-14 19:07:35 -0600

    Add color class

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
-var date = dayjs().format("H A");
+var date = dayjs().format("dddd, MMMM, D");
+var workHours = 8
+var workStartHour = 13
+var events = []
+
 
 
 $("#currentDay").text(date);
@@ -7,35 +11,41 @@ var timeCurrently = dayjs().hour()
 
 //funtion to save textbox content to local storage
 function saveLocal() {
-    var textbox = $(".textbox").val()
-    var hour = $(".hour").val()
-    localStorage.setItem(textbox, hour);
+    var savedEvent = {
+        textbox: $(".textbox").val(),
+        hour: $(".hour").val()
+    }
+    localStorage.setItem("savedEvent", savedEvent);
+
 }
 
 //button listner
 $(".saveBtn").on("click", saveLocal)
 
 //recipe for how to create a time block and inject in .container element
-var div1 = $("<div>").addClass("row time-block")
-var div2 = $("<div>").addClass("col-md-1 hour")
-var textarea = $("<textarea>").addClass("col-md-10 textbox future")
-var button = $("<button>").addClass("btn saveBtn col-md-1")
 
-button.append("<i class='fa fa-save'>")
-div1.append(div2).append(textarea).append(button);
-$(".container").append(div1);
 
-//for loop 8x
-//JSON.parse(localStorage.getItem(""))
-for (let i = 0; i < 8; i++) {
+//for loop that runs 8 times
+for (let i = 0; i < workHours; i++) {
+    //recipe for how to create a time block and inject in .container element
     var div1 = $("<div>").addClass("row time-block")
-    var div2 = $("<div>").addClass("col-md-1 hour")
-    var textarea = $("<textarea>").addClass("col-md-10 textbox future")
+    var div2 = $("<div>").addClass("col-md-1 hour").append(dayjs().hour(workStartHour + i).format("h A"))
+    var textarea = $("<textarea>").addClass("col-md-10 textbox")
     var button = $("<button>").addClass("btn saveBtn col-md-1")
     button.append("<i class='fa fa-save'>")
     div1.append(div2).append(textarea).append(button);
     $(".container").append(div1);
+    if (timeCurrently > (workStartHour + i)) { //past present future class change
+        textarea.addClass("past");
+    } else if (timeCurrently === (workStartHour + i)) {
+        textarea.addClass("present");
+    } else {
+        textarea.addClass("future");
+    }
 }
+
+
+
 
 
 
